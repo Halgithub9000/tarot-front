ae272d26fdcd078afd8192caf738c3bfca6ed851
csverinogpg <csverinogpg@gmail.com>
2025-06-20 20:23:08 -0400

    feat: add button to get spread

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,48 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import CartaTarot from "./components/CartaTarot";
 import { obtenerCartas } from "./services/tarotService";
 import "./App.css";
 
 function App() {
   const [cartas, setCartas] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const [intention, setIntention] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [tiradaMostrada, setTiradaMostrada] = useState(false);
 
-  useEffect(() => {
-    obtenerCartas()
-      .then(data => {
-        setCartas(data.cards);
-        setIntention(data.intention || "");
-        setLoading(false);
-      })
-      .catch(() => setLoading(false));
-  }, []);
+  const handleTirada = async () => {
+    setLoading(true);
+    try {
+      const data = await obtenerCartas();
+      setCartas(data.cards || []);
+      setIntention(data.intention || "");
+      setTiradaMostrada(true);
+    } catch {
+      setLoading(true);
+      alert("Error al obtener las cartas. Por favor, intenta de nuevo más tarde.");
+    }
+    setLoading(false);
+  };
 
   if (loading) return <div className="centro-pantalla">Cargando cartas...</div>;
 
   return (
     <div className="centro-pantalla">
-      <div className="fila-cartas">
-        {cartas.map((carta, idx) => (
-          <CartaTarot
-            key={idx}
-            nombre={carta.name }
-            pinta={carta.suit}
-            imagen="https://upload.wikimedia.org/wikipedia/commons/7/7f/RWS_Tarot_18_Moon.jpg"
-            reversed={carta.is_reversed}
-          />
-        ))}
-      </div>
+      {!tiradaMostrada ? (
+        <button onClick={handleTirada}>Mi Tirada Diaria</button>
+      ) : (
+        <div>
+          <div className="fila-cartas">
+            {Array.isArray(cartas) && cartas.map((carta, idx) => (
+              <CartaTarot
+                key={idx}
+                nombre={carta.name}
+                imagen={"https://upload.wikimedia.org/wikipedia/commons/7/7f/RWS_Tarot_18_Moon.jpg"}
+                reversed={carta.is_reversed}
+              />
+            ))}
+          </div>
+        </div>
+      )}
     </div>
   );
 }
